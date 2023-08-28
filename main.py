a1b551547aecee2bea909885269acc11c0954a26
Lucas Schulz <Lucas_schulz@sluz.ch>
2023-08-28 10:32:19 +0200

    ich konnte die Beispiels Daten extrahieren und im Terminal anzeigen lassen :>

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
 
-#URL
+# URL definieren
 url = 'https://www.wetter.ch/'
 
-#Get request
+# HTTP-GET-Anfrage durchführen
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 
+# Wolken-Elemente auf der Karte finden
 cloud_elements = soup.find_all('div', class_='bigmap_citybox')
 
+# Daten von jedem Wolken-Element extrahieren
 cloud_data = []
 
 for cloud in cloud_elements:
     style_content = cloud['style']
     image_url = style_content.split('background-image:url(')[1].split(')')[0]
-    location_link = cloud.find('div', class_='bigmap_city_tempsbox').find_next_sibling()['onclick'].split("'")[1]
+    location_link = cloud['onclick'].split("'")[1]
+    
     temp_low = cloud.find('div', class_='bigmap_city_templowbox').text
     temp_high = cloud.find('div', class_='bigmap_city_temphighbox').text
 
@@ -26,5 +29,6 @@ for cloud in cloud_elements:
         'HighTemp': temp_high
     })
 
+# Erste 5 Einträge ausgeben (nur zu Demonstrationszwecken)
 for data in cloud_data[:5]:
     print(data)
